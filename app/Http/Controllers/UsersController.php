abefa072f445ed0cefa28de06560050deb315716
dixitpatel89 <dixitpatel89@gmail.com>
2019-11-22 16:43:31 +0530

    2. POST requests with form fields "password", "id" and "comments" will append the value of 'comments' to the existing comments field of user with identifier 'id' provided the 'password' is a given static value.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,4 +22,26 @@ class UsersController extends Controller
 
         return view('user', ['user' => $userData]);
     }
+
+    public function update(Request $request, $id)
+    {
+        if (!is_numeric($id))
+            return response()->json('Invalid Id', 422);
+
+        foreach ($request->all() as $key => $data) {
+            if (empty($data))
+                return response()->json(ucfirst($key) . ' Required!', 422);
+        }
+
+        if ($request->password != "720DF6C2482218518FA20FDC52D4DED7ECC043AB")
+            return response()->json('Invalid password', 401);
+
+        $userData = User::find($id);
+        if (empty($userData))
+            return response()->json('User Not Exist!', 404);
+
+        $userData->update(['comments' => $userData->comments . '
+' . $request->comments]);
+        return response()->json('OK', 200);
+    }
 }
