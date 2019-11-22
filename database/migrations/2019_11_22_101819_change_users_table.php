88bd7668afc00c0356842501478588dd4d4f6202
dixitpatel89 <dixitpatel89@gmail.com>
2019-11-22 15:57:52 +0530

    Database migration/seed created for Users

---
+++ database/migrations/2019_11_22_101819_change_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn("email");
+            $table->dropColumn("email_verified_at");
+            $table->dropColumn("password");
+            $table->dropTimestamps();
+            $table->dropRememberToken();
+            $table->text('comments',120);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->dropColumn("comments");
+        });
+    }
+}
